1c2dd555d5f043251f6f43df5d3a2f7aa0435c2c
Srija S <srijakumar7@gmail.com>
2019-05-26 22:35:18 +0000

    Automatically backed up by Learn

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,7 +1,7 @@
 class AppointmentsController < ApplicationController
   def show
     @appointment = Appointment.find_by(:params[:id])
-      redirect_to appointment
+      
   end
   def index
     @appointments=Appointment.all
